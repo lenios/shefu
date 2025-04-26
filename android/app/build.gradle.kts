03a620878f7017fc141fec2ac02121b8dc072340
richard SELLAM <richard@orvidia.fr>
2025-04-26 16:34:46 +0200

    fix: use java17 for automated apk build and refresh android files

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "fr.orvidia.shefu"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "fr.orvidia.shefu"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+       create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = "key"
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+       }
+    }
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+dependencies {
+}
--- android/app/src/main/kotlin/fr/orvidia/shefu/MainActivity.kt
+++ android/app/src/main/kotlin/fr/orvidia/shefu/MainActivity.kt
@@ -2,5 +2,4 @@ package fr.orvidia.shefu
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
